4042784677833701d033393fbd609fdf8140133f
Oscar Delgado <30932646+kioselsa@users.noreply.github.com>
2022-09-19 21:02:17 -0500

    Creación de login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactsController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,5 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 })->name('inicio');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 //Rutas para contacto
 Route::get('contact/store',[ContactsController::class, 'store'])->name('contact.store');
+
+require __DIR__.'/auth.php';
